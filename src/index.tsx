423853a3b2827dcb1774404ecd6b840a637876ca
ShunIshizeki <shun2000c9@outlook.jp>
2024-01-27 12:28:37 +0900

    importにdestyleとindex.cssを追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styles/destyle.css";
 import "./styles/globals.scss";
+import "./styles/index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
