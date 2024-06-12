a5f05986e6d9c0ca44b0a03e02871d9f1b0a9722
ShunIshizeki <shun2000c9@outlook.jp>
2024-06-12 23:21:44 +0900

    【add】Snackbarコンポーネントをscreenに作成

--- src/components/screen/index.tsx
+++ src/components/screen/index.tsx
@@ -3,6 +3,7 @@ export { default as Header } from "./Header/Header";
 export { default as Loading } from "./Loading/Loading";
 export { default as PageTitle } from "./PageTitle/PageTitle";
 export { default as Section } from "./Section/Section";
+export { default as Snackbar } from "./Snackbar/Snackbar";
 export { default as Sticker } from "./Sticker/Sticker";
 export { default as Tag } from "./Tag/Tag";
 export { default as UpdatedAt } from "./UpdatedAt/UpdatedAt";
